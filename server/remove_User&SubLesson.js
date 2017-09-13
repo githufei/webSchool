e6a100276b71c7eb4ea2933b03b6cce24330984a
githufei <460284524@qq.com>
2017-09-13 11:33:46 +0800

    1

---
+++ server/remove_User&SubLesson.js
@@ -0,0 +1,21 @@
+﻿let { User, SubCourse } = require('./model');
+//情空用户信息和订阅列表
+SubCourse.remove({},function(err,result){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(result.result);
+        // {ok:1,n:2}
+        // ok=1表示的是删除操作成功,n=0表示实际删除掉的记录条数为2
+    }
+})
+
+User.remove({},function(err,result){
+    if(err){
+        console.log(err);
+    }else{
+        console.log(result.result);
+        // {ok:1,n:2}
+        // ok=1表示的是删除操作成功,n=0表示实际删除掉的记录条数为2
+    }
+})
---
+++ src/components/MHeader/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './index.less';
+import {withRouter} from 'react-router';
+// export default class MHeader extends Component {
+class MHeader extends Component {
+    back=()=>{
+        // this.props.history.goBack();
+        // window.history.back();
+        this.props.history.push('/');
+    }
+    render() {
+        return (
+            <div className="m-header">               
+                <div onClick={this.back} className="m-back"><i className="iconfont icon-back"></i></div>
+                <span>{this.props.title}</span>
+            </div>
+        )
+    }
+}
+export default withRouter(MHeader);
